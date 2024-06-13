bd96d63a2df45a282d461d0c7c9b912971de6584
Vincent C <gamemaker80@gmail.com>
2024-06-13 17:02:43 -0400

    Print 'X' to the screen to verify setup was done properly.

--- kernel.c
+++ kernel.c
@@ -7,6 +7,9 @@ int main(void)
 	// Text mode video address buffer:
 	unsigned short* vga_buffer = (unsigned short*)0x000B8000;
 
+	// Render a character to the screen to visually test the output:
+	vga_buffer[0] = 'X' | 0x0F << 8;
+
 	// Return as usual:
 	return 0;
 }
